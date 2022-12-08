b660c33bbafdef0fd7da1ee52a3f8108c7a78065
afshin <shahinof10009@gmail.com>
2022-12-08 14:35:57 +0330

    first look at option type

---
+++ src/bin/option-type.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+    let usr1: User = User { age: Some(21), name: "john".to_owned() };
+    println!("usr: name: {} , age: {:?}", usr1.name, usr1.age);
+}
+
+#[derive(Debug)]
+struct User {
+    age: Option<i8>,
+    name: String
+}
