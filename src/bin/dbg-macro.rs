4524e0e132c8fd8856ef63b707e022be57d879d0
afshin <shahinof10009@gmail.com>
2022-12-08 21:32:14 +0330

    dbg! macro tested

---
+++ src/bin/dbg-macro.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut a = "ello" ;
+    dbg!(a);
+    
+    a = "hello" ;
+    dbg!(a);
+
+    // out put of the file
+    // [src\bin\dbg-macro.rs:3] a = "ello" 
+    // [src\bin\dbg-macro.rs:7] a = "hello"(a);
+
+}
