d06eb46ea03ccfa64f87bcbbe60a2bc26237dbdf
afshin <shahinof10009@gmail.com>
2022-12-11 16:25:46 +0330

    add NeverZero struct to module number for encapsulation

--- src/bin/new-type-pattern.rs
+++ src/bin/new-type-pattern.rs
@@ -1,21 +1,27 @@
 // new type pattern allow code consumer like other developer 
 // use your struct with convinent manner (easy usage)
 
-struct NeverZero(i32) ;
-impl NeverZero {
-    fn new(n: i32) -> Result<Self, String> {
-        if n == 0 {
-            return Err("n can not be zero".to_owned())
+
+mod numbers {
+    pub struct NeverZero(i32) ;
+    impl NeverZero {
+        pub fn new(n: i32) -> Result<Self, String> {
+            if n == 0 {
+                return Err("n can not be zero".to_owned())
+            }
+            return Ok(NeverZero(n))
         }
-        return Ok(NeverZero(n))
+        pub fn get_value(&self) -> i32 {
+            self.0
+        }
     }
 }
 
 fn main() {
-    println!("{}", divide(10, NeverZero::new(0).unwrap()));
+    println!("{}", divide(10, numbers::NeverZero::new(2).unwrap()));
     // this code possiblly can crash  
 }
 
-fn divide(n1: i32, nz: NeverZero) -> i32 {
-    n1 / nz.0
+fn divide(n1: i32, nz: numbers::NeverZero) -> i32 {
+    n1 / nz.get_value()
 }
